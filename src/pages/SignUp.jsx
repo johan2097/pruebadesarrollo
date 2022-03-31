0c6fac4f16c764234bf5710eccb5f6ae10a5aba5
johan rodriguez morales <johanromorales97@gmail.com>
2022-03-31 13:10:39 -0500

    septimo  cambio

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
 import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import { Formik, Field, Form, ErrorMessage } from 'formik'
 import { FormHelperText } from '@material-ui/core'
@@ -49,40 +45,31 @@ const Signup = () => {
                     <Avatar style={avatarStyle}>
                         <AddCircleOutlineOutlinedIcon />
                     </Avatar>
-                    <h2 style={headerStyle}>Sign Up</h2>
-                    <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
+                    <h2 style={headerStyle}>Registrarse</h2>
+                    <Typography variant='caption' gutterBottom>Por favor crear la cuenta !</Typography>
                 </Grid>
                 <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
                     {(props) => (
                         <Form>
 
-                            <Field as={TextField} fullWidth name="name" label='Name'
-                                placeholder="Enter your name" helperText={<ErrorMessage name="name" />} />
-                            <Field as={TextField} fullWidth name="email" label='Email'
-                                placeholder="Enter your email" helperText={<ErrorMessage name="email" />} />
-                            <FormControl component="fieldset" style={marginTop}>
-                                <FormLabel component="legend">Gender</FormLabel>
-                                < Field as={RadioGroup} aria-label="gender" name="gender"  style={{ display: 'initial' }}>
-                                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                </ Field>
-                            </FormControl>
-                            <FormHelperText><ErrorMessage name="gender" /></FormHelperText>
-                            <Field as={TextField} fullWidth name="phoneNumber" label='Phone Number'
-                                placeholder="Enter your phone number" helperText={<ErrorMessage name="phoneNumber" />} />
-                            <Field as={TextField} fullWidth name='password' type="password"
-                                label='Password' placeholder="Enter your password"
+                            <Field as={TextField} fullWidth name="name" label='Nombre'
+                                placeholder="Nombre" helperText={<ErrorMessage name="name" />} />
+                            <Field as={TextField} fullWidth name="email" label='Apellido'
+                                placeholder="Apellido" helperText={<ErrorMessage name="email" />} />
+                    
+                            <Field as={TextField} fullWidth name="Email" label='Email'
+                                placeholder="Ingrese su email" helperText={<ErrorMessage name="email" />} />
+                            <Field as={TextField} fullWidth name='Contraseña' type="contraseña"
+                                label='Contraseña' placeholder="Enter your password"
                                 helperText={<ErrorMessage name="password" />} />
-                            <Field as={TextField} fullWidth name="confirmPassword" type="password"
-                                label='Confirm Password' placeholder="Confirm your password"
-                                helperText={<ErrorMessage name="confirmPassword" />} />
+                           
                             <FormControlLabel
                                 control={<Field as={Checkbox} name="termsAndConditions" />}
-                                label="I accept the terms and conditions."
+                                label="Acepto terminos y condiciones."
                             />
                             <FormHelperText><ErrorMessage name="termsAndConditions" /></FormHelperText>
                             <Button type='submit' variant='contained' disabled={props.isSubmitting}
-                                color='primary'>{props.isSubmitting ? "Loading" : "Sign up"}</Button>
+                                color='primary'>{props.isSubmitting ? "Loading" : "Registrarse"}</Button>
 
                         </Form>
                     )}
